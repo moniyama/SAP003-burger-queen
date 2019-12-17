750e2659f71f1ab2e4fdc789bd15f3cd0425ba3e
Monica Yamazaki <monicasayama@gmail.com>
2019-12-17 17:58:21 -0300

    add footer

--- burger-queen/src/App.js
+++ burger-queen/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import NavTab from './components/Nav-Tab';
+import Footer from './components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css'
@@ -10,6 +11,7 @@ export default function App() {
     <div className="App">
       <Header />
       <NavTab />
+      <Footer />
     </div>
   )
 }
---
+++ burger-queen/src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+
+const styles = StyleSheet.create({
+  footer: {
+    fontSize: "large",
+    borderRadius: "5px",
+    backgroundColor: "#F2B885",
+    fontWeight:'bold',
+    textAlign:'center',
+    width:'100%',
+    margin: '0px',
+    bottom:'0'
+  },
+})
+
+export default function Footer() {
+  return (
+    <footer className={css(styles.footer)}>
+      <p>Desenvolvido por MSAY </p>
+    </footer>
+    )
+}
