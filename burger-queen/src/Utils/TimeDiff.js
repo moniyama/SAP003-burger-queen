de20c3fb679e9fc29ea5f1ebc48721537c712f1b
Monica Yamazaki <monicasayama@gmail.com>
2020-01-19 20:58:41 -0300

    timediff as utils function

---
+++ burger-queen/src/Utils/TimeDiff.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function timeDiff(props) {
+  console.log(typeof props.timeDelivered);
+  console.log(typeof props.timeCooked);
+
+  const calculate = () => {
+    const inicialTime = new Date(props.timeOrdered).getTime();
+    let endTime;
+    props.page === "kitchen"
+      ? (endTime = new Date(props.timeCooked).getTime())
+      : (endTime = new Date(props.timeDelivered).getTime());
+    const microSecondsDiff = Math.abs(endTime - inicialTime);
+    const minDiff = Math.round(microSecondsDiff / (1000 * 60));
+    return <div>{`${minDiff} minutos`}</div>;
+  };
+
+  return props.timeDelivered !== undefined || props.timeCooked !== undefined
+    ? calculate()
+    : "";
+}
--- burger-queen/src/components/CardHistoric.js
+++ burger-queen/src/components/CardHistoric.js
@@ -2,40 +2,20 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, css } from "aphrodite";
 import Accordion from "react-bootstrap/Accordion";
 import HourFormate from "../Utils/HourFormate";
+import TimeDiff from "../Utils/TimeDiff";
 
 export default function CardHistoric(props) {
-  const [timeDiff, setTimeDiff] = useState(0);
-  const [endTime, setEndTime] = useState("");
-  const [initialTime, setInicialTime] = useState("");
-
-  const page = props.page;
-  const index = props.index;
-
-  useEffect(() => {
-    setInicialTime(new Date(props.order.timestamp_ordered).getTime());
-    page === "kitchen"
-      ? setEndTime(new Date(props.order.timestamp_cooked).getTime())
-      : setEndTime(new Date(props.order.timestamp_delivered).getTime());
-    const microSecondsDiff = Math.abs(endTime - initialTime);
-    const minDiff = Math.round(microSecondsDiff / (1000 * 60));
-    setTimeDiff(minDiff);
-  }, [
-    endTime,
-    initialTime,
-    page,
-    props.order.timestamp_cooked,
-    props.order.timestamp_delivered,
-    props.order.timestamp_ordered
-  ]);
-
   return (
     <Accordion defaultActiveKey="none">
-      <Accordion.Toggle eventKey={index} className={css(styles.accordion)}>
+      <Accordion.Toggle
+        eventKey={props.index}
+        className={css(styles.accordion)}
+      >
         <li
-          key={index}
+          key={props.index}
           className={css(
             styles.history,
-            index % 2 ? styles.colorOne : styles.colorTwo
+            props.index % 2 ? styles.colorOne : styles.colorTwo
           )}
         >
           <div className={css(styles.historyUserAndTime)}>
@@ -57,14 +37,19 @@ export default function CardHistoric(props) {
             )}
           </div>
           <div className={css(styles.historyTimeDiff)}>
-            {page === "kitchen"
+            {props.page === "kitchen"
               ? "Tempo de preparação:"
               : "Tempo de atendimento"}
-            <div>{timeDiff === isNaN ? "" : `${timeDiff} minutos`} </div>
+            <TimeDiff
+              page={props.page}
+              timeOrdered={props.order.timestamp_ordered}
+              timeCooked={props.order.timestamp_cooked}
+              timeDelivered={props.order.timestamp_delivered}
+            />
           </div>
         </li>
       </Accordion.Toggle>
-      <Accordion.Collapse eventKey={index}>
+      <Accordion.Collapse eventKey={props.index}>
         <section className={css(styles.show)}>
           {props.order.order.map((itemOrdered, index) => (
             <li key={"historyDetails" + index}>
